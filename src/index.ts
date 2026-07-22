cfd68f93fd8caaa3aab3b63e11e0b087b3027dfe
Douglas Silva <dougllassillva27@gmail.com>
2026-07-22 19:07:57 -0300

    feat(logger): adiciona timestamps em todos os logs do proxy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config'
+import { setupGlobalConsoleTimestamps } from './core/logger.js'
 import { startServer } from './api/server.js'
+
+setupGlobalConsoleTimestamps()
 
 startServer().catch(error => {
   console.error('Failed to start server:', error)
